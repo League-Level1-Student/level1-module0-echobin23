dd997f79278ad379b8b92010a5feea66dda468bb
echobin23 <league@WTS14.attlocal.net>
2018-07-25 19:55:44 -0700

    House, My First SwingGUI, CutenessTV IP

--- src/Houses.java
+++ src/Houses.java
@@ -1,28 +1,65 @@
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	static Robot Bob = new Robot();
-	 static int height = 0;
+	static int height = 0;
+
 	public static void main(String[] args) {
-		
+
 		Bob.setX(50);
 		Bob.setY(550);
 		Bob.penDown();
 		Bob.setSpeed(10);
-		for(int i = 0;i<10;i++) {
-			buildhouse("small");
+		for (int i = 0; i < 10; i++) {
+			buildhouse("large", "blue");
 		}
-		}
-	static void buildhouse(String size) {
-		if(size.equals("small")) {
+	}
+
+	static void buildhouse(String size, String color) {
+		if (size.equals("small")) {
 			height = 60;
-		}
-		if(size.equals("mediam")) {
+		} else if (size.equals("mediam")) {
 			height = 120;
-		}
-		if(size.equals("large")) {
+		} else if (size.equals("large")) {
 			height = 250;
 		}
+		if (color.equals("blue")) {
+			Bob.setPenColor(Color.BLUE);
+		} else if (color.equals("green")) {
+			Bob.setPenColor(Color.GREEN);
+		} else if (color.equals("red")) {
+			Bob.setPenColor(Color.RED);
+		} else {
+			Bob.setRandomPenColor();
+		}
+		Bob.setWindowColor(Color.BLACK);
+		Bob.move(height);
+		Bob.turn(90);
+		Bob.move(50);
+		Bob.turn(90);
+		Bob.move(height);
+		Bob.turn(270);
+		Bob.move(30);
+		Bob.turn(270);
+	}
+
+	static void drawPointyRoof() {
+		Bob.setWindowColor(Color.BLACK);
+		Bob.move(height);
+		Bob.turn(40);
+		Bob.move(40);
+		Bob.turn(90);
+		Bob.move(40);
+		Bob.move(height);
+		Bob.turn(270);
+		Bob.move(30);
+		Bob.turn(270);
+	}
+
+	static void drawFlatRoof() {
+		Bob.setWindowColor(Color.BLACK);
 		Bob.move(height);
 		Bob.turn(90);
 		Bob.move(50);
